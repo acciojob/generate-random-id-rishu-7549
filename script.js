d08b017f773b1e31d2e9c11b761652182767117c
Rishu Kumar <126085066+rishu-7549@users.noreply.github.com>
2025-02-04 16:50:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 function makeid(l) {
   // write your code here
+	let res = "";
+	let char = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890";
+	for (let i = 0; i < l; i++) {
+		res += char.charAt(Math.floor(Math.random()*char.length));
+	}
+	return res;
 }
 
 // Do not change the code below.
 const l = prompt("Enter a number.");
 alert(makeid(l));
+
+
